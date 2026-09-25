7f25982a7e4090e06d8ad6d97d2d7d27f9e6eaf7
Tobias <328139456+Taboudi@users.noreply.github.com>
2026-09-25 20:16:33 +0200

    Added basic tournament page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Cat Royale</h1>
-      <p>My first website project.</p>
+
+      <h2>16 cats enter, Only the cutest will be crowned.</h2>
+
+      <p>
+        In Cat Royale, you choose your favourite cat in every duel and discover your ultimate
+        champion of cuteness.
+      </p>
+
+      <Link href="/play">
+        Start Royale
+      </Link>
     </main>
   );
 }
---
+++ src/app/play/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Play() {
+     return (
+    <main>
+      <Link href="/">← Back to Cat Royale</Link>
+
+      <h1>Cat Royale</h1>
+
+      <p>Round of 16</p>
+
+      <h2>Choose your favourite cat</h2>
+
+      <p>The first duel will appear here.</p>
+    </main>
+  );
+}
